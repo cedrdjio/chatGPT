5f1fba158c0947d0b24f3ac5f7aea89d65b4e397
cedrdjio <cericnzoyem@gmail.com>
2022-12-31 19:58:04 +0100

    end for day

--- client/script.js
+++ client/script.js
@@ -38,8 +38,8 @@ function chatStrip(isAi, value, uniqueId) {
   return `
         <div class="wrapper ${isAi && " ai"}">
             <div class="chat">
-                <div className="profile">
-                <img src="${isAi ? bot : user}" alt="${isAi ? " bot" : "user" }" />
+                <div class="profile">
+                <img src="${isAi ? bot : user}" alt="${isAi ? " bot" : "user"}" />
                 </div>
                 <div class="message" id = ${uniqueId}>${value}</div>
             </div>
@@ -48,22 +48,43 @@ function chatStrip(isAi, value, uniqueId) {
 }
 
 const handleSubmit = async (e) => {
-    e.preventDefault();
-    const data = new FormData(form);
+  e.preventDefault();
+  const data = new FormData(form);
 
-    //user's chatstripe
-    chatContainer.innerHTML += chatStrip(false, data.get("prompt"));
-    form.reset();
-    //bot's chatstripe
-    const uniqueId = generateUniqueId();
-    chatContainer.innerHTML += chatStrip(true, " ", uniqueId);
-    const botMessage = document.getElementById(uniqueId);
-    loader(botMessage);
+  //user's chatstripe
+  chatContainer.innerHTML += chatStrip(false, data.get("prompt"));
+  form.reset();
+  //bot's chatstripe
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStrip(true, " ", uniqueId);
+  const botMessage = document.getElementById(uniqueId);
+  loader(botMessage);
+
+  //fetching data from server
+  const response = await fetch("http://localhost:3000/", {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({
+      prompt: data.get("prompt"),
+    }),
+  });
+  console.log(data.get("prompt"));
+  clearInterval(loadInterval);
+  botMessage.innerHTML = "";
+  if(response.ok){
+    const data = await response.json();
+    typeText(botMessage, data.bot.trim());
+  }else{
+    const err = await response.json();
+    botMessage.innerHTML = "Something went wrong";
+    console.log(err);
+    // alert(err);
+  }
 };
 
 form.addEventListener("submit", handleSubmit);
 form.addEventListener("keyup", (e) => {
-    if(e.key==="Enter") {
-        handleSubmit(e);
-    }
+  if (e.key === "Enter") {
+    handleSubmit(e);
+  }
 });
--- server/server.js
+++ server/server.js
@@ -3,8 +3,9 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 import { Configuration , OpenAIApi } from 'openai';
 
-dotenv.config();
+dotenv.config("process.env.OPENAI_API_KEY");
 
+console.log(process.env.OPENAI_API_KEY);
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
@@ -20,26 +21,30 @@ app.get('/', async (req, res) => {
     });
 });
 
-app.post('/api', async (req, res) => {
-    try {
-        const { prompt } = req.body.prompt;
-        const response = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: `${prompt}`,
-            temperature: 0,
-            max_tokens: 3000,
-            top_p: 1,
-            frequency_penalty: 0.5,
-            presence_penalty: 0,
-        });
-        res.status(200).send({
-            bot: response.data.choices[0].text});
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({error});
-    }   
-    });
+app.post('/', async (req, res) => {
+    if (req.body && req.body.prompt) {
+        try {
+            const prompt  = req.body.prompt;
+            const response = await openai.createCompletion({
+                model: "text-davinci-003",
+                prompt: `${prompt}`,
+                temperature: 0,
+                max_tokens: 3000,
+                top_p: 1,
+                frequency_penalty: 0.5,
+                presence_penalty: 0,
+            });
+            res.status(200).send({
+                bot: response.data.choices[0].text});
+        } catch (error) {
+            console.log(error);
+            res.status(500).send({error});
+        }   
+    } else {
+        res.status(400).send({error: 'Invalid request body'});
+    }
+});
 
-    app.listen(3000, () => {
-        console.log('Server is running on port 3000');
-    });
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
